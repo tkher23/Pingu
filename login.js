a7eaf14dfaf1ccd1ea73a4fc29f185c8009fbf1b
tkher23 <tanishkher@gmail.com>
2025-06-08 01:30:02 -0700

    Import createClient from supabase-js module

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
 const SUPABASE_URL = "https://ishnglghmfijbgtuhxzd.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzaG5nbGdobWZpamJndHVoeHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Nzk5ODIsImV4cCI6MjA2NDA1NTk4Mn0.WmapiFoeezlJ0v5rqHBl3gedsbRZmhvWeL_x_2U_vcI";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createClient } from "@supabase/supabase-js"
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 document.getElementById("loginBtn").addEventListener("click", async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
